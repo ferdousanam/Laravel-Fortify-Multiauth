3b00841fe7e9a5ba7756190d377a822e73b8f820
Ferdous Anam <ferdous.anam@gmail.com>
2021-01-21 19:38:46 +0600

    Multi Auth Reset Password Feature

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Admin\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Admin\Auth\NewPasswordController;
+use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,6 +24,10 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
     Route::group(['middleware' => ['guest:admin']], function () {
         Route::get('/login', [AuthenticatedSessionController::class, 'create']);
         Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login');
+        Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
+        Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
+        Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
+        Route::post('/reset-password', [NewPasswordController::class, 'store'])->name('password.update');
     });
 
     Route::group(['middleware' => ['auth:admin']], function () {
