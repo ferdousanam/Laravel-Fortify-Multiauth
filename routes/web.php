719dcc04694a838a285443cbbf2399f44f2a1cf0
Ferdous Anam <ferdous.anam@gmail.com>
2021-01-21 12:10:36 +0600

    Laravel Fortify Setup with Bootstrap Scaffolding

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
